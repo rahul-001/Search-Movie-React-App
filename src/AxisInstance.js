e5242be40bcd589f3b589cd10ac5ef96f3ed0d35
rahul078 <rahul.sumhr@gmail.com>
2020-10-01 09:27:49 +0530

    Add env file

--- src/AxisInstance.js
+++ src/AxisInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const token = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZTdkN2JjOTAxOWM1MjQzZGEyYWFjZmVmZTdmOWE3NiIsInN1YiI6IjVmNzRhMDk3YmM4YWJjMDAzODZkYjliZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BUDf5J3CHadMvsyct7dsdrjSEi0CufsUnJQlX6Xmrys';
+export const token = process.env.REACT_APP_API_TOKEN;
 
 const ConvertHeadersForBackend = (headers) => {
     headers.Authorization = token;
